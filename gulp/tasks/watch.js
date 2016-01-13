5f505b546df9ac853a46a484fca41371eaa4a854
Alexey Bondarenko <alexeybondarenko@me.com>
2016-01-13 19:04:15 +0200

    fix slick height

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -18,8 +18,8 @@ gulp.task('delete', function (cb) {
     rimraf('./'+config.dest.root, cb);
 });
 gulp.task('default', ['server', 'watch'], function() {});
-gulp.task('build', ['html','font','sprite','copy','js','sass'], function() {});
-gulp.task('deploy', function() {
-    gulp.src('./build/**/*')
+gulp.task('build', ['html', 'jade','font','sprite','copy','js','sass'], function() {});
+gulp.task('deploy', ['build'], function() {
+    return gulp.src('./build/**/*')
         .pipe(ghPages());
 });
