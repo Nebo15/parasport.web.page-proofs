fabbcb26feb9d2d89d282ca8b040c43a81138011
Alexey Bondarenko <alexeybondarenko@me.com>
2016-01-13 21:12:53 +0200

    fix derver

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -17,7 +17,7 @@ gulp.task('watch', [
 gulp.task('delete', function (cb) {
     rimraf('./'+config.dest.root, cb);
 });
-gulp.task('default', ['server', 'watch'], function() {});
+gulp.task('default', ['build', 'server', 'watch'], function() {});
 gulp.task('build', ['html', 'jade','font','sprite','copy','js','sass'], function() {});
 gulp.task('deploy', ['build'], function() {
     return gulp.src('./build/**/*')
